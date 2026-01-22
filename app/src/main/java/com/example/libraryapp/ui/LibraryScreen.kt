0cba9e7fdf859b1946dc17e173a2b397271c9b1b
Carlos Bermúdez Mancebo <carlosbermudezmancebo@gmail.com>
2026-01-22 18:46:38 +0100

    Screen improved

--- app/src/main/java/com/example/libraryapp/ui/LibraryScreen.kt
+++ app/src/main/java/com/example/libraryapp/ui/LibraryScreen.kt
@@ -7,8 +7,12 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.ElevatedCard
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -18,11 +22,15 @@ import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ModifierLocalBeyondBoundsLayout
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.libraryapp.data.AppDatabase
+import com.example.libraryapp.data.Book
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -56,20 +64,37 @@ fun LibraryScreen(
                 Column(
                     modifier = Modifier
                         .padding(16.dp)
-                        .fillMaxWidth()
-                        .border(1.dp, Color.Black)
                 ) {
-                    Text(
-                        text = book.title,
-                        fontWeight = FontWeight.Bold
-                    )
-                    Text(
-                        text = book.author,
-                    )
+                    Libro(book)
                 }
             }
         }
     }
+}
+
+@Composable
+fun Libro(book: Book) {
+    ElevatedCard(
+        elevation = CardDefaults.cardElevation(
+            defaultElevation = 6.dp
+        ),
+        modifier = Modifier
+            .size(width = 240.dp, height = 100.dp)
+    ) {
+        Text(
+            text = book.title,
+            modifier = Modifier
+                .padding(8.dp),
+            textAlign = TextAlign.Center,
+        )
+        Text(
+            text = book.author,
+            modifier = Modifier
+                .padding(8.dp),
+            textAlign = TextAlign.Center,
+        )
+    }
+
 }
 
 //@Preview(showBackground = true)
